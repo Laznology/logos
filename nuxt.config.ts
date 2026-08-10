e658e140bf28df256671c897cf264e510f8e9d1f
LazTech <laznology.dev@gmail.com>
2026-08-10 18:52:58 +0700

    chore(config): add sqlite dialect configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2026-06-30",
-
   css: ["~/assets/css/main.css"],
-
   devtools: {
     enabled: true,
   },
-
   hub: {
     blob: true,
+    db: {
+      dialect: "sqlite",
+    },
   },
 
   modules: [
